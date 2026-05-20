d5358c181aaf47f153286b3ea05ff5a0d3ea9ac1
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-05-20 04:18:23 +0200

    feat(div): add v4 no-nop n4 call skip bridge

--- EvmAsm/Evm64/DivMod.lean
+++ EvmAsm/Evm64/DivMod.lean
@@ -50,3 +50,4 @@ import EvmAsm.Evm64.DivMod.Compose.ModFullPathN3LoopUnified
 import EvmAsm.Evm64.DivMod.Compose.FullPathN2Bundle
 import EvmAsm.Evm64.DivMod.Compose.FullPathN4NoNop
 import EvmAsm.Evm64.DivMod.Compose.FullPathN4V4NoNop
+import EvmAsm.Evm64.DivMod.Compose.FullPathN4CallV4NoNop
--- EvmAsm/Evm64/DivMod/LoopIterN4CallV4NoNop.lean
+++ EvmAsm/Evm64/DivMod/LoopIterN4CallV4NoNop.lean
@@ -21,6 +21,18 @@ def loopBodyN4CallSkipJ0PreV4
     v0 v1 v2 v3 u0 u1 u2 u3 uTop qOld retMem dMem dloMem scratchUn0 **
   (sp + signExtend12 3936 ↦ₘ scratchMem)
 
+theorem loopBodyN4CallSkipJ0PreV4_unfold
+    {sp jOld v5Old v6Old v7Old v10Old v11Old v2Old
+     v0 v1 v2 v3 u0 u1 u2 u3 uTop qOld
+     retMem dMem dloMem scratchUn0 scratchMem : Word} :
+    loopBodyN4CallSkipJ0PreV4 sp jOld v5Old v6Old v7Old v10Old v11Old v2Old
+      v0 v1 v2 v3 u0 u1 u2 u3 uTop qOld retMem dMem dloMem scratchUn0 scratchMem =
+    (loopBodyN4CallSkipJ0Pre sp jOld v5Old v6Old v7Old v10Old v11Old v2Old
+      v0 v1 v2 v3 u0 u1 u2 u3 uTop qOld retMem dMem dloMem scratchUn0 **
+    (sp + signExtend12 3936 ↦ₘ scratchMem)) := by
+  unfold loopBodyN4CallSkipJ0PreV4
+  rfl
+
 @[irreducible]
 def loopBodyN4CallSkipJ0PostV4
     (sp base v0 v1 v2 v3 u0 u1 u2 u3 uTop scratchMem : Word) : Assertion :=
